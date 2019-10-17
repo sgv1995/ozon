f173ff32848fac1b54e5d6d252962c1c6c4e62e4
Grisha <grisha5991@gmail.com>
2019-10-18 00:56:14 +0300

    Fixed #4 Перезагрузил класс продукт

--- src/main/java/ru/itpark/Product.java
+++ src/main/java/ru/itpark/Product.java
@@ -1,33 +1,33 @@
- package ru.itpark;
+package ru.itpark;
 
- public class Product {
-     private int id;
-     private String name;
-     private String description;
-     private int fullPrice;
-     private int discountPercentage;
-     private int rating;
-     private String imageUrl;
-     private boolean available;
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private int fullPrice;
+    private int discountPercentage;
+    private int rating;
+    private String imageUrl;
+    private boolean available;
 
 
-     public Product(int id, String name, String description, int fullPrice, int discountPercentage, int rating, String imageUrl, boolean available) {
-         this.id = id;
-         this.name = name;
-         this.description = description;
-         this.fullPrice = fullPrice;
-         this.discountPercentage = discountPercentage;
-         this.rating = rating;
-         this.imageUrl = imageUrl;
-         this.available = available;
-     }
+    public Product(int id, String name, String description, int fullPrice, int discountPercentage, int rating, String imageUrl, boolean available) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.fullPrice = fullPrice;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.imageUrl = imageUrl;
+        this.available = available;
+    }
 
-     public int getFullPrice() {
+    public int getFullPrice() {
         return fullPrice;
     }
-     public int getDiscountPercentage() {
+    public int getDiscountPercentage() {
         return discountPercentage;
     }
-     public boolean getAvailable() { return available; }
-     public void setAvailable(boolean available) { this.available = available; }
- }
+    public boolean getAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+}
